0a433000064e0fd273990a02ad91d583d3af5f49
YurneroLA <YurneroLA@gmail.com>
2013-08-15 14:58:53 +0800

    init push

---
+++ iter.lua
@@ -0,0 +1,13 @@
+function list_iter (t)
+	local i = 0
+	local n = table.getn(t)
+	return function ()
+		i = i + 1
+		if i <= n then return t[i] end
+	end
+end
+
+t = {10, 20, 30}
+for element in list_iter(t) do
+	print(element)
+end
